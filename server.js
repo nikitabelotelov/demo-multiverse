f724e469098aae85273565074e848d52685e816a
nikitabelotelov <nikbelotelov@gmail.com>
2021-10-26 22:09:15 +0300

    add static server

---
+++ server.js
@@ -0,0 +1,13 @@
+let static = require('node-static')
+let http = require('http')
+
+let file = new static.Server('./dist')
+
+const PORT = 8080
+
+http.createServer(function (request, response) {
+  request.addListener('end', function () {
+    file.serve(request, response)
+  }).resume()
+}).listen(PORT)
+console.log(`Server started on port: ${PORT}`)
